d04f8f8dcc3bd61480418437f5bea1cccda5ed5e
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-07-19 18:34:47 -0300

    refactor: adicionando params no useCase listar produtos

--- src/applications/useCase/product/List.ts
+++ src/applications/useCase/product/List.ts
@@ -1,10 +1,27 @@
+import { InternalServerErrorException } from '@nestjs/common';
+import {
+  FindAllParams,
+  PaginatedResponse,
+} from 'src/core/domain/fornecedor/repository/Fornecedor.repository';
 import { Product } from 'src/core/domain/product/entity/Product';
 import { ProductRepository } from 'src/core/domain/product/repository/ProductRepository';
+import { Either, left, right } from 'src/shared/either';
+
+type ResponseClient = Either<
+  InternalServerErrorException,
+  PaginatedResponse<Product>
+>;
 
 export class ListAllUseCase {
   constructor(private readonly productRepository: ProductRepository) {}
 
-  async execute(): Promise<Product[]> {
-    return await this.productRepository.findAll();
+  async execute(params: FindAllParams): Promise<ResponseClient> {
+    try {
+      const result = await this.productRepository.findAll(params);
+      return right(result);
+    } catch (error: any) {
+      console.error('Error fetching Products:', error);
+      return left(new InternalServerErrorException('Failed to list Products.'));
+    }
   }
 }
